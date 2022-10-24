6568da8a9c75b7d6c8007b49ba47ad951b844064
DoronKabaso10 <doronkabaso10@gmail.com>
2022-10-24 17:15:23 +0300

    change app name and version number

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter auth Demo',
+      title: 'הזמנת מגרשים',
       home: Login(),
     );
   }
